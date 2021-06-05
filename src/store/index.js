b873b0729f1786437a22566a2e916d797ab246c8
PJozsef <pallagijoe@gmail.com>
2021-06-05 21:02:54 +0200

    remove authReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from './taskReducer'
-import authReducer from "./authReducer";
 
 const store = configureStore({
   reducer: {
     todo: taskReducer,
-    auth: authReducer
   },
 });
 
